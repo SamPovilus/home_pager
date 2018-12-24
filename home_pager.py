9e5845d389ef66d545460e72cacc4e2182f8d078
Sam Povilus <kernel.development@povil.us>
2018-12-24 11:09:01 -0700

    starting to add raspberry pi gpio stuff

--- home_pager.py
+++ home_pager.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python3
 import sys
+from gpiozero import RGBLED, Button
+from time import sleep
 
-class Params(object):
-    """ A class to hold the parameters for the home pager """
-    MAIN_CONSOLE_RECEVE_PORT = 32323
-    MAIN_CONSOLE_SEND_PORT   = 32324
     
 class HomePager(object):
+    led1 = RGBLED(red=22,green=27,blue=17,active_high = False)
+    switch = Button(23,pull_up=False,bounce_time=0.02)
     is_main_console = False
     def main():
         if(sys.argv[1] == "main"):
@@ -15,7 +15,12 @@ class HomePager(object):
             print("am remote console")
         print(sys.argv[1])
         print(Params.MAIN_CONSOLE_SEND_PORT)
-  
+
+        class Params(object):
+            """ A class to hold the parameters for the home pager """
+            MAIN_CONSOLE_RECEVE_PORT = 32323
+            MAIN_CONSOLE_SEND_PORT   = 32324
+
 if __name__== "__main__":
   HomePager.main()
 
